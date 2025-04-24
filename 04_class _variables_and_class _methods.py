cb72a7139589f4e181da70a256b61a1b0bd7d8df
Muhammad Bilal Hussain <bilalhussain42201@gmail.com>
2025-04-24 09:39:22 -0700

    update

--- 04_class _variables_and_class _methods.py
+++ 04_class _variables_and_class _methods.py
@@ -1,3 +1,10 @@
+
+"""
+! 4. Class Variables and Class Methods
+Assignment:
+Create a class Bank with a class variable bank_name. Add a class method change_bank_name(cls, name) that allows changing the bank name. Show that it affects all instances.
+"""
+
 class Bank:
     bank_name = "Default Bank"  
 
